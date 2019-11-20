cc32544ed259d0b6c11f31a6cf4a123a3e721f34
Bjverde <bjverde@yahoo.com.br>
2019-11-20 02:01:19 -0300

    :hammer: verão 1.3.0

---
+++ v130/geradorLista.js
@@ -0,0 +1,350 @@
+function palavrasSimples() {
+  var palavrasSimples = ['Ave'
+                        ,'Arara'
+                        ,'Avestur'
+                        ,'Acre'
+                        ,'Banana'
+                        ,'Bacana'
+                        ,'Babaca'
+                        ,'Carro'
+                        ,'Carne'
+                        ,'Dado'
+                        ,'Gabinte'
+                        ,'Gabrieala'
+                        ,'Gado'
+                        ,'Gato'
+                        ,'Gafanhoto'
+                        ,'Geladeira'
+                        ,'Gaivota'
+                        ,'Galera'
+                        ,'Galeria'
+                        ,'Game'
+                        ,'Gama'
+                        ,'Ganho'
+                        ,'Garganta'
+                        ,'Gari'
+                        ,'Garimpo'
+                        ,'Galo'
+                        ,'Nabo'
+                        ,'Nação'
+                        ,'Nádega'
+                        ,'Nadir'
+                        ,'Nariz'
+                        ,'Nascer'
+                        ,'Nata'
+                        ,'Natação'
+                        ,'Bumbum'
+                        ];
+  return palavrasSimples;
+}
+
+function palavrasComplexas() {
+  var palavrasComplexas = ['Abduzir'
+                          ,'Acre'
+                          ,'Acrimônia'
+                          ,'Acusados'
+                          ,'Adolescente'
+                          ,'Adstrito'
+                          ,'Advocacia'
+                          ,'Anticonstitucionalissimamente'
+                          ,'Aeronáutica'
+                          ,'Aliciar'
+                          ,'Belicoso'
+                          ,'Capcioso'
+                          ,'Chistoso'
+                          ,'Coalizão'
+                          ,'Cominar'
+                          ,'Consociação'
+                          ,'Constituiente'
+                          ,'Corolário'
+                          ,'Dimetilaminofenildimetilpirazolona'
+                          ,'Dissentir'
+                          ,'Eflúvio'
+                          ,'Elucubrações'
+                          ,'Empedernido'
+                          ,'Engodar'
+                          ,'Estado'
+                          ,'Exórdio'
+                          ,'Hipopotomonstrosesquipedaliofobia'
+                          ,'Homizio'
+                          ,'Idiossincrasia'
+                          ,'Igneo'
+                          ,'Influição'
+                          ,'Inconstitucionalissimamente'
+                          ,'Jaez'
+                          ,'Lauto'
+                          ,'Loquaz'
+                          ,'Mediatário'
+                          ,'Municipio'
+                          ,'Nato'
+                          ,'Naturalizado'
+                          ,'Próbrio'
+                          ,'Oftalmotorrinolaringologista'
+                          ,'Oscular'
+                          ,'Paraclorobenzilpirrolidinonetilbenzimidazol'
+                          ,'Pecúlio'
+                          ,'Perdulário'
+                          ,'Pernóstico'
+                          ,'Piperidinoetoxicarbometoxibenzofenona'
+                          ,'Plaga'
+                          ,'Pneumoultramicroscopicossilicovulcanoconió'
+                          ,'Procrastinar'
+                          ,'Prolegômenos'
+                          ,'Quimera'
+                          ,'Republicano'
+                          ,'Rubicundo'
+                          ,'Tetrabrometacresolsulfonoftaleína'
+                          ,'Tergiversar'
+                          ,'Ufanismo'
+                          ,'União'
+                          ,'Vicissitudes'
+                          ,'Vitupério'
+                        ];
+  return palavrasComplexas;
+}
+
+
+/***
+ * Origem de varias palavras
+ * https://www.trtsp.jus.br/consultas/221-pagina-principal/atendimento-e-servicos-trt2/1422-glossario-de-termos-juridicos
+ **/
+function palavrasConstituicao() {
+  var palavrasConstituicao = ['Abuso de poder'
+                        ,'Abuso sexual'
+                        ,'Acusados'
+                        ,'Administração Pública'
+                        ,'Adolescente'
+                        ,'Advocacia'
+                        ,'Advogado'
+                        ,'Aeronáutica'
+                        ,'Aliciar'
+                        ,'Anticonstitucionalissimamente'
+                        ,'Agravo'
+                        ,'Citação'
+                        ,'Comunicação'
+                        ,'Conflito de competência'
+                        ,'Constituiente'
+                        ,'Constituição'
+                        ,'Correição'
+                        ,'Defesa'
+                        ,'Desembargador'
+                        ,'Despacho'
+                        ,'Dilação'
+                        ,'Diligência'
+                        ,'Distrito Federal'
+                        ,'Dissídio'
+                        ,'Efeito suspensivo'
+                        ,'Embargos'
+                        ,'Ex nunc'
+                        ,'Ex officio'
+                        ,'Ex tunc'
+                        ,'Estado'
+                        ,'Estatutário'
+                        ,'Férias'
+                        ,'Foro'
+                        ,'Garantias Fundamentais'
+                        ,'Habeas corpus'
+                        ,'Habeas data'
+                        ,'Honorários'
+                        ,'Independência'
+                        ,'Instância'
+                        ,'Intempestivo'
+                        ,'Jurisdição'
+                        ,'Jurisprudência'
+                        ,'Jus postulandi'
+                        ,'Licitação'
+                        ,'Liminar'
+                        ,'Litigante de má fé'
+                        ,'Litisconsórcio'
+                        ,'Litispendência'
+                        ,'Mandado judicial'
+                        ,'Mandado de segurança'
+                        ,'Medida cautelar'
+                        ,'Mérito da ação'
+                        ,'Militares'
+                        ,'Municipio'
+                        ,'Nacional'
+                        ,'Nato'
+                        ,'Naturalizado'
+                        ,'Notificação'
+                        ,'Obreiro'
+                        ,'Oficial de justiça'
+                        ,'Ônus da prova'
+                        ,'Ouvidoria'
+                        ,'Parecer'
+                        ,'Partes'
+                        ,'Perícia'
+                        ,'Plantão judiciário'
+                        ,'Precatório'
+                        ,'Preliminar'
+                        ,'Preposto'
+                        ,'Prescrição'
+                        ,'Processo'
+                        ,'Processos pendentes'
+                        ,'Procuraçãoad judicia'
+                        ,'Provimento'
+                        ,'Recesso'
+                        ,'Reclamante'
+                        ,'Recolhimento previdenciário'
+                        ,'Recurso ordinário'
+                        ,'Redução a termo'
+                        ,'Relator'
+                        ,'Republicano'
+                        ,'República Federativa do Brasil'
+                        ,'Revelia'
+                        ,'Revisor'
+                        ,'Rito'
+                        ,'Rito (ou procedimento) sumário'
+                        ,'Rito (ou procedimento) sumaríssimo'
+                        ,'Segredo de justiça'
+                        ,'Sessão de julgamento'
+                        ,'Sindicato'
+                        ,'Sobrestado'
+                        ,'Sobreaviso'
+                        ,'Substabelecimento'
+                        ,'Sucumbência'
+                        ,'Súmula'
+                        ,'Suspeição'
+                        ,'Sustentação oral'
+                        ,'Trânsito em julgado'
+                        ,'Turma'
+                        ,'Tutela'
+                        ,'Ulterior'
+                        ,'União'
+                        ,'Vara do Trabalho'
+                        ,'Vínculo empregatício'
+                        ,'Voto'
+                        ,'Zonas'
+                        ];
+  return palavrasConstituicao;
+}
+
+function palavrasComputacao() {
+  var palavrasComputacao = ['Access Point'
+                          ,'ADSL'
+                          ,'Adware'
+                          ,'Antivírus'
+                          ,'Arquivos'
+                          ,'Artefato'
+                          ,'Assinatura digital'
+                          ,'Autoridade certificadora'
+                          ,'Backdoor'
+                          ,'Banda'
+                          ,'Bandwidth'
+                          ,'Bluetooth'
+                          ,'Bot'
+                          ,'Cable modem'
+                          ,'Cavalo de tróia'
+                          ,'Certificados'
+                          ,'Código'
+                          ,'Código malicioso'
+                          ,'Computador'
+                          ,'Internet'
+                          ,'Monitor'
+                          ,'Mouse'
+                          ,'Notebook'
+                          ,'Rede'
+                          ,'Rede sem fio'
+                          ,'Software'
+                          ,'Software livre'
+                          ,'Spyware'
+                          ,'Teclado'
+                          ,'Transmissão de dados'
+                          ,'Virus'
+                          ,'Worms'
+                        ];
+  return palavrasComputacao;
+}
+
+function checksIsTrue(aChk) {
+      var result = false;
+      var count = 0;
+	    for (var i=0;i<aChk.length;i++){
+	        if (aChk[i].checked == true){
+	            count = count + 1;
+	        }
+	    }
+      if(count > 0){
+        result = true;
+      }
+      return result;
+	}
+
+function arrayRemoveDuplicados(array) {
+  var novaArr = array.filter(function(este, i) {
+      return array.indexOf(este) == i;
+  });
+  return  novaArr;
+}
+
+function getListPalavras(checkPalavras) {
+  var palavras = [];
+  if(checkPalavras[0].checked == true){
+    var listPalavras = palavrasSimples();
+    palavras = palavras.concat(listPalavras);
+  }
+  if(checkPalavras[1].checked == true){
+    var listPalavras = palavrasComplexas();
+    palavras = palavras.concat(listPalavras);
+  }
+  if(checkPalavras[2].checked == true){
+    var listPalavras = palavrasConstituicao();
+    palavras = palavras.concat(listPalavras);
+  }
+  if(checkPalavras[3].checked == true){
+    var listPalavras = palavrasComputacao();
+    palavras = palavras.concat(listPalavras);
+  }
+  return palavras;
+}
+
+function getLista(qtdpalavras,checkPalavras) {
+  var lista = getListPalavras(checkPalavras);
+  var selected = [];
+  if(lista.length==0){
+    document.getElementById("lista").innerHTML = "ERRO: Palavras não encontradas";
+  }else{
+    do{
+      var index = Math.floor(Math.random() * lista.length);
+      selected.push(lista[index]);
+      selected = arrayRemoveDuplicados(selected);
+    }while(selected.length < qtdpalavras);
+  }
+  return selected;
+}
+
+function geraDivListaHtmml(tipLista,lista) {
+  var divListaHtmml = ' ';
+  for (var i = 0; i < lista.length; i++){
+      divListaHtmml = divListaHtmml+'<li>'+lista[i]+'</li>';
+  }
+  if(tipLista==true){
+    divListaHtmml = '<ul>'+divListaHtmml+'</ul>';
+  }else{
+    divListaHtmml = '<ol>'+divListaHtmml+'</ol>';
+  }
+  return divListaHtmml;
+}
+
+function limparLista() {
+    document.getElementById("lista").innerHTML = "";
+}
+
+function gerarLista() {
+  var checkPalavras = document.getElementsByName('dicionario');
+  if(checksIsTrue(checkPalavras)==false){
+    document.getElementById('lista').innerHTML = "ERRO: INFORME UM TIPO DE PALAVRA";
+  }else{
+    var qtdpalavras = document.getElementById('qtdpalavras').value;
+    var lista       = getLista(qtdpalavras,checkPalavras);
+    var ordem    = document.getElementsByName('ordem');
+    var ordemAlfabetica = ordem[1].checked;
+    if(ordemAlfabetica){
+      lista = lista.sort();
+    }
+    var tipLista    = document.getElementsByName('tipLista');
+    var tipListaTopico = tipLista[0].checked;
+    var divListaHtmml = geraDivListaHtmml(tipListaTopico,lista);
+    document.getElementById("lista").innerHTML = divListaHtmml;
+  }
+}
